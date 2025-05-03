9e9e1a99393f2921a2ed6b749849b0910555551d
vinicius <viniciusmul98@gmail.com>
2025-05-03 09:27:36 -0300

    init

---
+++ src/test/java/botty/botty/BottyApplicationTests.java
@@ -0,0 +1,13 @@
+package botty.botty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
